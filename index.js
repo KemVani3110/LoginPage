3836ccf14a87537190dc03f393092df84561a257
BP <chuminhkhoi3110@gmail.com>
2023-02-02 15:55:46 +0700

    Page Login

---
+++ index.js
@@ -0,0 +1,19 @@
+let randomNumber = Math.round(Math.random(0,1) * 100); 
+let so 
+
+do
+{
+    so = prompt("Nhap vao so bi an");
+    if(so == randomNumber)
+        {
+            alert('Bạn đã đoán đúng');
+        }
+    else if( randomNumber < so)
+    {   
+        alert('Số bạn nhập nhỏ hơn số bí ẩn');
+    }
+    else{
+        alert('Số bạn nhập lớn hơn số bí ẩn');
+    }
+}
+while(randomNumber != so)
