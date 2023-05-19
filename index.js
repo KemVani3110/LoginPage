89e2f4e561010f4e6ecc34f1b80ed30d41e292f2
BP <chuminhkhoi3110@gmail.com>
2023-05-19 12:14:25 +0700

    fix all login page

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-let randomNumber = Math.round(Math.random(0,1) * 100); 
-let so 
+// let randomNumber = Math.round(Math.random(0,1) * 100); 
+// let so 
 
-do
-{
-    so = prompt("Nhập vào số bí ẩn");
-    if(so == randomNumber)
-        {
-            alert('Bạn đã đoán đúng');
-        }
-    else if( randomNumber < so)
-    {   
-        alert('Số bạn nhập nhỏ hơn số bí ẩn');
+// do
+// {
+//     so = prompt("Nhập vào số bí ẩn");
+//     if(so == randomNumber)
+//         {
+//             alert('Bạn đã đoán đúng');
+//         }
+//     else if( randomNumber < so)
+//     {   
+//         alert('Số bạn nhập nhỏ hơn số bí ẩn');
+//     }
+//     else{
+//         alert('Số bạn nhập lớn hơn số bí ẩn');
+//     }
+// }
+// while(randomNumber != so)
+
+function towerOfHanoi(n, fromRod, toRod, auxRod) {
+    if (n === 1) {
+      console.log(`Move disk 1 from ${fromRod} to ${toRod}`);
+      return;
     }
-    else{
-        alert('Số bạn nhập lớn hơn số bí ẩn');
-    }
-}
-while(randomNumber != so)
+    towerOfHanoi(n - 1, fromRod, auxRod, toRod);
+    console.log(`Move disk ${n} from ${fromRod} to ${toRod}`);
+    towerOfHanoi(n - 1, auxRod, toRod, fromRod);
+  }
+  
+  // Sử dụng hàm:
+  towerOfHanoi(3, 'A', 'C', 'B'); // Di chuyển 3 đĩa từ cọc A sang cọc C, sử dụng cọc B làm cọc trung gian
+  
